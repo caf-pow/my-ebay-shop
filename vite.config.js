af8c3b58fee742cd9d82bd07af5494f3638274ad
caf-pow <ibiswrx555+01@gmail.com>
2026-04-18 08:58:38 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-ebay-shop/',
+  // どんなリポジトリ名でも動くように、相対パス（./）を徹底します
+  base: './', 
+  build: {
+    outDir: 'dist',
+  }
 })
