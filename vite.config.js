e39dd7e3685666b04ada1197e358563f1f51fda8
caf-pow <ibiswrx555+01@gmail.com>
2026-04-18 08:55:17 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // これを入れることで、GitHub Pages上での画像やCSSの読み込みパスを正しく調整します
+  base: '/my-ebay-shop/',
 })
