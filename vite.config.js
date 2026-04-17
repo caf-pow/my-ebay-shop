0e18aeb943ac14832272619cc9317cc42ea9e5a1
caf-pow <ibiswrx555+01@gmail.com>
2026-04-18 08:43:24 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', //
+  base: './', // これを入れることで、GitHub Pages上での画像やCSSの読み込みパスを正しく調整します
 })
