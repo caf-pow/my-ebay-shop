6d340b4a862c031baf69ad59cf6042909b22a84f
caf-pow <ibiswrx555+01@gmail.com>
2026-04-18 09:13:18 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // どんなリポジトリ名でも動くように、相対パス（./）を徹底します
-  base: './', 
-  build: {
-    outDir: 'dist',
-  }
-})
+export default {
+  base: './',
+}
